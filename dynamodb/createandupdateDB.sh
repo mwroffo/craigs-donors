3a600e3dae5b02c074009e320200eff931821bd6
Mohit Gadkari <MohitGadkari@fico.com>
2020-06-20 08:58:53 -0700

    Integrated with API gateway

--- dynamodb/createandupdateDB.sh
+++ dynamodb/createandupdateDB.sh
@@ -3,7 +3,7 @@ set -ex
 
 
 create_json_dynamodb(){
-curl -X GET --header "Accept: application/json" "https://api.data.charitynavigator.org/v2/Organizations?app_id=186c7d3c&app_key=5b6f6b0f5897d612e3d21636f294404a&pageSize=10&pageNum=50&rated=true&fundraisingOrgs=true&sizeRange=1" > input_charity.json 
+curl -X GET --header "Accept: application/json" "curl -X GET --header "Accept: application/json"  https://iws9pqk8ui.execute-api.us-east-1.amazonaws.com/v1/getcordinates?sitename=mohitapi" > input_charity.json 
 json-dynamo-putrequest charity input_charity.json --beautify > output_charity.json
 aws dynamodb batch-write-item --request-items file://output_charity.json
 echo "Updated Charity DB Table"
