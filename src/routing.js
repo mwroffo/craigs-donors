17445bb0a788994e7167ef21c61e0ef195730dcb
Michael W Roffo <mwroffo@gmail.com>
2020-06-29 17:42:54 -0400

    fresh start
    Signed-off-by: Michael W Roffo <mwroffo@gmail.com>

--- src/routing.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const {Route} = require('react-router')
-
-module.exports = <Route component={App}>
-    <Route path="/home" component={Home} exact={true} />
-    <Route exact path="/" render={() => <Redirect to="/home" />} />
-</Route>
