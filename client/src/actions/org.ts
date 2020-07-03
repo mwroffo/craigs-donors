8eb8e11d2882369a66138dcb1dd892aa7819c6a6
Michael W Roffo <mwroffo@gmail.com>
2020-07-03 10:20:02 -0400

    added ionic to client/package.json, renamed orgs.js to org.js
    Signed-off-by: Michael W Roffo <mwroffo@gmail.com>

---
+++ client/src/actions/org.ts
@@ -0,0 +1,8 @@
+// org.ts
+// define functions which:
+//     get json objects from the db
+//     via the express routes in ./routes,
+//     then finally dispatch them to React.
+
+import api from '../utils/api';
+
